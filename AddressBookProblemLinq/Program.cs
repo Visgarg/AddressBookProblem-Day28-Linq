18c24df1109d72518811fdf6bc135669636dc628
Vishal Garg <vishugarg10004@gmail.com>
2020-11-09 00:06:16 +0530

    [Vishal] added same data for multiple type and used switch case.

--- AddressBookProblemLinq/Program.cs
+++ AddressBookProblemLinq/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Text;
 
 namespace AddressBookProblemLinq
 {
@@ -69,7 +70,7 @@ namespace AddressBookProblemLinq
             DataColumn[] PrimaryKeyColumns = new DataColumn[2];
             PrimaryKeyColumns[0] = addressBookTable.Columns["firstName"];
             PrimaryKeyColumns[1] = addressBookTable.Columns["phoneNumber"];
-            addressBookTable.PrimaryKey = PrimaryKeyColumns;
+           // addressBookTable.PrimaryKey = PrimaryKeyColumns;
 
             //Inserting data into columns into datatable UC3
             addressBookTable.Rows.Add("Vishal", "Garg", "Barwala", "Hisar", "Haryana", 125121, 8570934858, "vishal.garg","Vishal","family");
@@ -78,41 +79,77 @@ namespace AddressBookProblemLinq
             addressBookTable.Rows.Add("Kamalakar", "Singh", "Thanesar", "Kurukshetra", "Haryana", 136119, 9595959595, "kamalakar.singh","Vishal","family");
             addressBookTable.Rows.Add("Praveen", "Kumar", "rohini", "Delhi", "Delhi", 435121, 7897897898, "praveen.kumar","Mahak","family");
             addressBookTable.Rows.Add("Apoorva", "Singh", "Andheri", "Mumbai", "Maharashtra", 125445, 8598598599, "apporva.singh","Mahak","Business");
-            
+            addressBookTable.Rows.Add("Praveen", "Kumar", "rohini", "Delhi", "Delhi", 435121, 7897897898, "praveen.kumar", "Mahak", "Business");
+
             AddressBookManagement addressBookManagement = new AddressBookManagement();
-            //UC4
-            //addressBookTable = addressBookManagement.UpdatedContactDetails(addressBookTable);
-            ////var book = addressBookTable.AsEnumerable().Select(r => r.Field<string>("state"));
-            //Console.WriteLine("*******************Total data*****************");
-            //foreach (var data in addressBookTable.AsEnumerable())
-            //{
-            //    Console.WriteLine("FirstName:- " + data.Field<string>("firstName"));
-            //    Console.WriteLine("lastName:- " + data.Field<string>("lastName"));
-            //    Console.WriteLine("Address:- " + data.Field<string>("address"));
-            //    Console.WriteLine("City:- " + data.Field<string>("city"));
-            //    Console.WriteLine("State:- " + data.Field<string>("state"));
-            //    Console.WriteLine("zip:- " + Convert.ToInt32(data.Field<int>("zip")));
-            //    Console.WriteLine("phoneNumber:- " + Convert.ToDouble(data.Field<Double>("phoneNumber")));
-            //    Console.WriteLine("eMail:- " + data.Field<string>("eMail"));
-            //    Console.WriteLine("***************");
-            //}
-            //deleting contacts from address book table UC5
-            //addressBookTable= addressBookManagement.DeletingContactFromTable(addressBookTable);
-            //checking if contact is deleted 
-            //Console.WriteLine(addressBookTable.Rows);
-            //var book = addressBookTable.AsEnumerable().Select(r => r.Field<string>("firstName"));
-            //foreach (string element in book)
-            //{
-            //    Console.WriteLine(element);
-            //}
-            //UC6
-            //addressBookManagement.RetrievingContactDetailsByStateOrCity(addressBookTable);
-            //UC7
-            //addressBookManagement.GetCountByCityAndState(addressBookTable);
-            //UC8
-            //addressBookManagement.GetSortedDataBasedOnPersonName(addressBookTable);
-            //UC10
-            addressBookManagement.GetCountByType(addressBookTable);
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Please press 1 to update the details");
+                Console.WriteLine("Please press 2 for deleting the contact");
+                Console.WriteLine("Please press 3 for retrieving contact details by state or city");
+                Console.WriteLine("Please press 4 for getting count by city and state");
+                Console.WriteLine("Please press 5 for getting sorted data based on person name for particular city");
+                Console.WriteLine("Please press 6 for getting count by type ");
+                Console.WriteLine("press any other digit to exit");
+                string options =Console.ReadLine();
+                switch (options)
+                {
+                    case "1":
+                        //UC4
+                        addressBookTable = addressBookManagement.UpdatedContactDetails(addressBookTable);
+                        //var book = addressBookTable.AsEnumerable().Select(r => r.Field<string>("state"));
+                        Console.WriteLine("*******************Total data*****************");
+                        foreach (var data in addressBookTable.AsEnumerable())
+                        {
+                            Console.WriteLine("FirstName:- " + data.Field<string>("firstName"));
+                            Console.WriteLine("lastName:- " + data.Field<string>("lastName"));
+                            Console.WriteLine("Address:- " + data.Field<string>("address"));
+                            Console.WriteLine("City:- " + data.Field<string>("city"));
+                            Console.WriteLine("State:- " + data.Field<string>("state"));
+                            Console.WriteLine("zip:- " + Convert.ToInt32(data.Field<int>("zip")));
+                            Console.WriteLine("phoneNumber:- " + Convert.ToDouble(data.Field<Double>("phoneNumber")));
+                            Console.WriteLine("eMail:- " + data.Field<string>("eMail"));
+                            Console.WriteLine("***************");
+                        }
+                        break;
+                    case "2":
+                        //deleting contacts from address book table UC5
+                        addressBookTable = addressBookManagement.DeletingContactFromTable(addressBookTable);
+                        //checking if contact is deleted
+                        Console.WriteLine(addressBookTable.Rows);
+                        var book = addressBookTable.AsEnumerable().Select(r => r.Field<string>("firstName"));
+                        foreach (string element in book)
+                        {
+                            Console.WriteLine(element);
+                        }
+                        break;
+                    case "3":
+                        //UC6
+                        addressBookManagement.RetrievingContactDetailsByStateOrCity(addressBookTable);
+                        break;
+                    case "4":
+                        //UC7
+                        addressBookManagement.GetCountByCityAndState(addressBookTable);
+                        break;
+                    case "5":
+                        //UC8
+                        addressBookManagement.GetSortedDataBasedOnPersonName(addressBookTable);
+                        break;
+                    case "6":
+                        //UC10
+                        addressBookManagement.GetCountByType(addressBookTable);
+                        break;
+                    default:
+                        Environment.Exit(0);
+                        break;
+
+                }
+            }
+
+           
+            
+            
         }
     }
 }
